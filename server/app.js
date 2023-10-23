4c1a41e4c71994d2d216b8fe6639fad7cf1ef150
naphatchanun <111549617+naphatchanun@users.noreply.github.com>
2023-10-23 18:06:44 +0700

    update

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const { MongoClient } = require("mongodb");
+const cors = require('cors')
+const userModel = require('./models/signup')
+
+const app = express()
+const port = 3000
+const uri = ('mongodb+srv://onosannnnt:admin123@cluster0.zx9nnzi.mongodb.net/oh-mypet')
+app.use(express.json())
+app.use(cors())
+
+app.get('/')
